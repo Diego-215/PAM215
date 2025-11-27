7e9248747a890ea0d62c53ee5c5a447a5f37f56a
Diego-215 <124049121@upq.edu.mx>
2025-11-26 23:07:24 -0600

    Insert y Select con SQLite Local
    Practica completada

---
+++ InsertYSelect/App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import InsertUsuarioScreen from './screens/InsertUsuarioScreen';
+import { UsuarioController } from './controllers/UsuarioController';
+
+export default function App() {
+  
+  return <InsertUsuarioScreen/>                             
+  
+}
